ef49847b9eeaf6d7a983cc410d2cd88885607e3a
Timothy <yourname@example.comtimkhumpan@gmail.com>
2023-02-05 01:01:03 -0500

    update

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -16,7 +16,7 @@ const generateAction = async (req, res) => {
     model: "text-davinci-003",
     prompt: `${basePromptPrefix}${req.body.userInput}\n`,
     temperature: 0.7,
-    max_tokens: 100,
+    max_tokens: 250,
   });
 
   const basePromptOutput = baseCompletion.data.choices.pop();
